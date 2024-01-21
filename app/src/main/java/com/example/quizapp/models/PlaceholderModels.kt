89754a7ea316ed75a3bba47ca6af2a898d9ab063
igorniebylski <igor.niebylski@gmail.com>
2024-01-21 21:44:20 +0100

    add imageUrl property

--- app/src/main/java/com/example/quizapp/models/PlaceholderModels.kt
+++ app/src/main/java/com/example/quizapp/models/PlaceholderModels.kt
@@ -5,12 +5,14 @@ import java.time.LocalDateTime
 data class Quiz  (
     val id:String,
     val title:String,
+    val imageUrl:String?,
     val desc:String,
 )
 
 data class Question (
     val id:String,
     val text:String,
+    val imageUrl:String?,
     val options:List<String>,
     val correct:Int,
 )
